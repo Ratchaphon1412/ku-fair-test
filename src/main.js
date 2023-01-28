66b10317d787259734b7d9c329410624af210ff4
Ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-01-28 14:29:07 +0700

    use flowbite and syncfusion

--- src/main.js
+++ src/main.js
@@ -1,14 +1,21 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import {registerLicense} from '@syncfusion/ej2-base';
+
+registerLicense('ORg4AjUWIQA/Gnt2VVhkQlFadVdJX3xLeUx0RWFab196d1JMYFRBNQtUQF1hSn5SdE1jWHxWc3FQQ2Rf');
+
+
 import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
 
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
 
 app.mount('#app')
